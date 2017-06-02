bb72dc70913490619920d12fe3e221d8b41f45db
kirill.shpin <kirill.shpin@gmail.com>
2017-06-02 10:21:03 -0700

    Initial commit

---
+++ app/src/main/java/pro/shpin/kirill/eulex/Select.java
@@ -0,0 +1,15 @@
+package pro.shpin.kirill.eulex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Select extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select);
+
+
+	}
+}
